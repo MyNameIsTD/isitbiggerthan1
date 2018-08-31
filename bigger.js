f0d386a87c9c6335dbfbf375bff1eff749f7c9e6
Teriyaki <30562709+MyNameIsTD@users.noreply.github.com>
2018-08-31 22:26:08 +1200

    Create bigger.js

---
+++ bigger.js
@@ -0,0 +1,13 @@
+function isit(number) {
+  if (number > 1) {
+    console.log("oh no it is bigger")
+  } else if (number < 1) {
+    console.log("lol it's small")
+  } else if (number === 1) {
+    console.log("dude it's the same thing.")
+  } else {
+    console.log("put a number there boi")
+  }
+}
+
+isit() //put number here
